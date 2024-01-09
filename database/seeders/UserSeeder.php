c273a7281ce3e3db4e8f0179a0ac083016761391
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-01-09 16:03:19 -0300

    add RoleSeeder and UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::create([
+            'name'=>'admin',
+            'email'=>'admin@mail.cl',
+            'password'=>Hash::make('1234')
+        ]);
+        $admin->assignRole('admin');
+
+        $editor = User::create([
+            'name'=>'user',
+            'email'=>'user@mail.cl',
+            'password'=>Hash::make('1234')
+        ]);
+
+        $editor->assignRole('editor');
+    }
+}
