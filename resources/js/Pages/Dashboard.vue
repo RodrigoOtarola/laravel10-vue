4312953c9f2bc553f1f5aaaec5a4a75da18f3dc1
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2024-01-09 17:54:05 -0300

    update Pages/Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 </script>
 
 <template>
@@ -14,7 +15,8 @@ import Welcome from '@/Components/Welcome.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+                    <SecondaryButton v-if="$page.props.user.permissions.includes('create roles')"> Crear Roles</SecondaryButton>
+                    <SecondaryButton v-if="$page.props.user.permissions.includes('create lessons')"> Crear Clases</SecondaryButton>
                 </div>
             </div>
         </div>
